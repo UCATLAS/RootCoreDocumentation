3db1768235b7d1c13d96c347f62726b9ac912702
Giordon Stark <kratsg@gmail.com>
2017-04-15 14:17:16 -0500

    fix up, add explicit git pull

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@ doxygen
 ls
 git remote -v
 git checkout master
-git status
+git pull
+git status -uno
 mkdir -p docs/${ABV}
 mv temp/${ABV}/html/* docs/${ABV}/.
 mv temp/rootcore_${ABV}.tag tagfiles/rootcore_${ABV}.tag
@@ -15,11 +16,12 @@ ls docs/${ABV}
 ls tagfiles
 git add docs/${ABV} tagfiles/*${ABV}.tag docs/index.html releases
 git commit -m "Adding docs for ${ABV}" --author="xAH-bot <giordon.holtsberg.stark@cern.ch>"
-git status
+git status -uno
 # this part handles moving the xml over
 git checkout xml
+git pull
 mkdir -p xml/${ABV}
 mv temp/${ABV}/xml/* xml/${ABV}/.
 git add xml/${ABV}
 git commit -m "Adding xml for ${ABV}" --author="xAH-bot <giordon.holtsberg.stark@cern.ch>"
-git status
+git status -uno
