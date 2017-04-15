ce91eb7b1d303739975acf70bf32bfcf6f9927d9
Giordon Stark <kratsg@gmail.com>
2017-04-14 20:55:12 -0500

    add run.sh script

---
+++ run.sh
@@ -0,0 +1,18 @@
+doxygen
+ls
+git checkout master
+git remote -v
+git status
+mv temp/${ABV}/html docs/${ABV}
+mv temp/rootcore_${ABV}.tag tagfiles/rootcore_${ABV}.tag
+echo ${ABV} >> releases
+sort -ur releases
+> docs/index.html
+for release in releases; do echo "<li><a href='${release}/'>${release}</a></li>" >> docs/index.html; done
+ls docs/
+ls docs/${ABV}
+ls tagfiles
+git add docs/${ABV} tagfiles/*${ABV}.tag docs/index.html releases
+git status
+git commit -m "Adding docs for ${ABV}" --author="xAH-bot <giordon.holtsberg.stark@cern.ch>"
+git status
