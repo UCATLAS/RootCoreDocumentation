1152a80577e683df1ba23a4ce2eeca75d84f7f5e
Giordon Stark <kratsg@gmail.com>
2017-04-14 22:00:00 -0500

    fix duplicates in releases file

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ git status
 mv temp/${ABV}/html docs/${ABV}
 mv temp/rootcore_${ABV}.tag tagfiles/rootcore_${ABV}.tag
 echo ${ABV} >> releases
-sort -ur releases
+sort -ur -o releases releases
 > docs/index.html
 for release in `cat releases`; do echo "<li><a href='${release}/'>${release}</a></li>" >> docs/index.html; done
 ls docs/
