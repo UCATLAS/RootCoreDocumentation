60c0f80556ded4e77d0fccda3a740b2c54f49d53
Giordon Stark <kratsg@gmail.com>
2017-04-15 19:59:42 -0500

    silence git

--- run.sh
+++ run.sh
@@ -6,13 +6,13 @@ git status -uno
 # make directories and copy over
 #mkdir -p xml/${ABV}
 mkdir -p docs/${ABV}
-#git rm -rf xml/${ABV}/*
-git rm -rf docs/${ABV}/*
+#git rm -rf xml/${ABV}/* &> /dev/null
+git rm -rf docs/${ABV}/* &> /dev/null
 #cp -R temp/${ABV}/xml/* xml/${ABV}/.
 cp -R temp/${ABV}/html/* docs/${ABV}/.
 cp -R temp/rootcore_${ABV}.tag tagfiles/rootcore_${ABV}.tag
 # add xml
-#git add xml/${ABV} > /dev/null
+#git add xml/${ABV} &> /dev/null
 #git commit -m "Adding xml for ${ABV}" --author="xAH-bot <giordon.holtsberg.stark@cern.ch>"
 # update releases ran
 echo ${ABV} >> releases
@@ -21,6 +21,6 @@ sort -ur -o releases releases
 > docs/index.html
 for release in `cat releases`; do echo "<li><a href='${release}/index.html'>${release}</a></li>" >> docs/index.html; done
 # add html
-git add docs/${ABV} tagfiles/*${ABV}.tag docs/index.html releases > /dev/null
+git add docs/${ABV} tagfiles/*${ABV}.tag docs/index.html releases &> /dev/null
 git commit -m "Adding docs for ${ABV}" --author="xAH-bot <giordon.holtsberg.stark@cern.ch>"
 git status -uno
